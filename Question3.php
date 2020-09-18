b0406e98c7615b7e2459df0dcdef303081cf2aba
Farisani <43832307+Farisani@users.noreply.github.com>
2020-09-18 14:56:53 +0200

    Update Question3.php

--- Question3.php
+++ Question3.php
@@ -1,31 +1,42 @@
+//Question 3 a
 <?php
 
-function search($arr, $prefix){
 
-    $number;
+function get_destination($phone_number)
+{
 
-    foreach($arr as $key => $data){
-     if($prefix == $data){
-         $number = $key;
-     }
-    
+    $data = check_destination($phone_number);
+
+    foreach ($data as $results){
+        $output = $results;
     }
-    return $number;
 
+     return $output;
+    }
+
+function check_destination($phone_number)
+{
+
+    //read from the database using the string phone_number and the sql statement
+
+    $mysql = new mysqli('localhost', 'root', '','prefixmap');
+
+    $SQL = "select destination from prefixMap where prefix =  " . $phone_number;
+
+    $results = $mysql->query($SQL);
+
+    if($results->num_rows == 0){
+        $output = "Invalid Phone Number";
+    }
+    else{
+        $output = $results->fetch_row();
+
+    }
+
+    return $output;
 }
 
-function searchDest($arr, $dest){
-
-    $number;
-
-    foreach($arr as $key => $data){
-     if($dest == $data){
-         $number = $data;
-     }
-    
-    }
-    return $number;
-}
 
 
-?>
+//Question 3 a
+
